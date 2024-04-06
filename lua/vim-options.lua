685e759f830e17b9e1f0d06547f95f0c333bad7b
Matheus Rodrigues <mrodrigues@foro.io>
2024-04-05 23:31:08 -0300

    Initial neo vim configurations

---
+++ lua/vim-options.lua
@@ -0,0 +1,9 @@
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set autoindent")
+vim.g.mapleader = " "
